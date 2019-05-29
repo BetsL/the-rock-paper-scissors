281ba8bb6d566eec0c6ed81ebae83f158a2f1388
Bets <elizabeth.lidstrom@gmail.com>
2019-05-28 20:48:54 -0400

    win function set

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 
 
 // cache the DOM; HTML variables that store DOM elements get tagged with an underscore, woo!
-const userScore = 0;
-const computerScore = 0;
+
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
-const result_div = document.querySelector('.result');
+const result_p = document.querySelector('.result > p');
 const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
 
 
 // functions :p
+
+
+// opponent behavior/cosmos
 function getComputerChoice(){
 	const choices = ['r', 'p', 's'];
-	const randomNumber = Math.floor(Math.random() * 3);
+	const randomNumber = Math.floor(Math.random() * 3);	
+	// b/c there are three possible outcomes
 	return choices[randomNumber];
 }
 
-function win(){
-	console.log('win');
+function convertToWord(letter){
+	if( letter === 'r' ) return 'Rock';
+	if( letter === 'p' ) return 'Paper';
+	return 'Scissors';
+}
+
+function win(user, compy){
+	userScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_p.innerHTML = convertToWord(user) + ' beats ' + convertToWord(compy) + '. You win!';
 }
 
 function lose(){
@@ -31,28 +45,30 @@ function draw(){
 	console.log('draw');
 }
 
+
+// user behavior/cosmos
 function game(userChoice){
 	const computerChoice = getComputerChoice();
 	switch(userChoice + computerChoice){
 		case 'rs':
 		case 'pr':
 		case 'sp':
-			win();
+			win(userChoice, computerChoice);
 			break;
 		case 'rp':
 		case 'ps':
 		case 'sr':
-			lose();
+			lose(userChoice, computerChoice);
 			break;
 		case 'rr':
 		case 'pp':
 		case 'ss':
-			draw();
+			draw(userChoice, computerChoice);
 			break;
 	}
 }
 
-function main(){	// get it all into its own little cosmos
+function main(){	// get it all into its own little cosmos!
 
 	rock_div.addEventListener('click', function(){
 		game('r');
