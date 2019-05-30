4a48ccc04fef39fc3198e0dd27ed96d5d3b00990
Bets <elizabeth.lidstrom@gmail.com>
2019-05-30 06:45:25 -0400

    updated functions to reflect es6 standards

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-
 // cache the DOM; HTML variables that store DOM elements get tagged with an underscore, woo!
-
 
 let userScore = 0;
 let computerScore = 0;
@@ -13,9 +11,7 @@ const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
 
-
 // functions :p
-
 
 // opponent behavior/cosmos
 function getComputerChoice(){
@@ -25,45 +21,55 @@ function getComputerChoice(){
 	return choices[randomNumber];
 }
 
+// return human-readable strings in place of variables/letters
 function convertToWord(letter){
 	if( letter === 'r' ) return 'Rock';
 	if( letter === 'p' ) return 'Paper';
 	return 'Scissors';
 }
 
+
+// winning conditions + behavior
 function win(user, compy){
 	const smallUserWord = "user".fontsize(3).sub();
 	const smallCompWord = "comp".fontsize(3).sub();
-
 	const user_div = document.getElementById(user);
+
 	userScore++;
 	userScore_span.innerHTML = userScore;
 	computerScore_span.innerHTML = computerScore;
 	result_p.innerHTML = `${convertToWord(user)}${smallUserWord} beats ${convertToWord(compy)}${smallCompWord}. You win!`;
 	user_div.classList.add('green-glow');
-	setTimeout(function() { user_div.classList.remove('green-glow')}, 1000);
+	
+	setTimeout(() => user_div.classList.remove('green-glow'), 1000);
 }
 
-
+// losing conditions + behavior
 function lose(user, compy){
 	const smallUserWord = "user".fontsize(3).sub();
 	const smallCompWord = "comp".fontsize(3).sub();
+	const user_div = document.getElementById(user);
 
 	computerScore++;
 	userScore_span.innerHTML = userScore;
 	computerScore_span.innerHTML = computerScore;
 	result_p.innerHTML = `${convertToWord(user)}${smallUserWord} loses to ${convertToWord(compy)}${smallCompWord}. You lost.`;
 	user_div.classList.add('red-glow');
-	setTimeout(function() { user_div.classList.remove('red-glow')}, 1000);
+	
+	setTimeout(() => user_div.classList.remove('red-glow'), 1000);
 }
 
+
+// what about the draw?
 function draw(user, compy){
 	const smallUserWord = "user".fontsize(3).sub();
 	const smallCompWord = "comp".fontsize(3).sub();
-	
+	const user_div = document.getElementById(user);
+
 	result_p.innerHTML = `${convertToWord(user)}${smallUserWord} equals ${convertToWord(compy)}${smallCompWord}. It's a draw`;
 	user_div.classList.add('grey-glow');
-	setTimeout(function() { user_div.classList.remove('grey-glow')}, 1000);
+	
+	setTimeout(() => user_div.classList.remove('grey-glow'), 1000);
 }
 
 
@@ -90,18 +96,9 @@ function game(userChoice){
 }
 
 function main(){	// get it all into its own little cosmos!
-
-	rock_div.addEventListener('click', function(){
-		game('r');
-	})
-
-	paper_div.addEventListener('click', function(){
-		game('p');
-	})
-
-	scissors_div.addEventListener('click', function(){
-		game('s');
-	})
+	rock_div.addEventListener('click', () => game('r'));
+	paper_div.addEventListener('click', () => game('p'));
+	scissors_div.addEventListener('click', () => game('s'));
 }
 
 main();
